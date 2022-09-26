c9da9a85a6eb19c2b59dc6ee34e2e26cd2025670
lana2-2karla <allanakarla1702@gmail.com>
2022-09-26 16:38:14 -0300

    :white_check_mark: test: classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    result_product = Product(
+        8,
+        "Aspirin",
+        "Galena Biopharma",
+        "2021-02-22",
+        "2024-03-14",
+        "KZ63 800H NM4B ZOWB YYUI",
+        "instrucao 8"
+    )
+
+    assert result_product.id == 8
+    assert result_product.nome_do_produto == "Aspirin"
+    assert result_product.nome_da_empresa == "Galena Biopharma"
+    assert result_product.data_de_fabricacao == "2021-02-22"
+    assert result_product.data_de_validade == "2024-03-14"
+    assert result_product.numero_de_serie == "KZ63 800H NM4B ZOWB YYUI"
+    assert result_product.instrucoes_de_armazenamento == "instrucao 8"
